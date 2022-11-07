6527fa4947f2496cf4ec9003ba5d3ead1129fed3
ZeeshanTahir786 <hafizzeeshan138@gmail.com>
2022-11-07 18:45:50 +0500

    Create PlaywithDialogs.java

---
+++ src/test/java/appium/PlaywithDialogs.java
@@ -0,0 +1,17 @@
+package appium;
+
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PlaywithDialogs extends BaseTest {
+    @Test
+    public void dialogs() {
+        driver.findElement(AppiumBy.accessibilityId("App")).click();
+        driver.findElement(AppiumBy.accessibilityId("Alert Dialogs")).click();
+        driver.findElement(By.id("io.appium.android.apis:id/two_buttons")).click();
+        Assert.assertTrue(driver.findElement(By.id("android:id/button1")).isDisplayed());
+        driver.findElement(By.id("android:id/button1")).click();
+    }
+}
