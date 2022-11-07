a71e75aa82b563af55d88f1fa22ba7bd77e4e28c
ZeeshanTahir786 <hafizzeeshan138@gmail.com>
2022-11-07 16:04:00 +0500

    Create ScrollDemo.java

---
+++ src/test/java/appium/ScrollDemo.java
@@ -0,0 +1,27 @@
+package appium;
+
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+
+public class ScrollDemo extends BaseTest {
+    @Test
+    public void scrollDemoTest() {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+
+//      this method is used when scroll is known : it uses the Google engine
+//        driver.findElement(AppiumBy.androidUIAutomator(("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"))")));
+
+//      When scroll is not known : It's appium official method
+
+        boolean canScrollMore;
+        do {
+            canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                    "left", 100, "top", 100, "width", 200, "height", 200,
+                    "direction", "down",
+                    "percent", 3.0
+            ));
+        } while ((canScrollMore));
+    }
+}
