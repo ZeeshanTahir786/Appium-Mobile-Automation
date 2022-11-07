6676f182869d73406f35289db506bd2bc12c0792
ZeeshanTahir786 <hafizzeeshan138@gmail.com>
2022-11-07 17:27:54 +0500

    Create DragDrop.java

---
+++ src/test/java/appium/DragDrop.java
@@ -0,0 +1,29 @@
+package appium;
+
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DragDrop extends BaseTest {
+    @Test
+    public void dragDrop() {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        driver.findElement(AppiumBy.accessibilityId("Drag and Drop")).click();
+        WebElement droppableElem = driver.findElement(By.id("io.appium.android.apis:id/drag_dot_1"));
+
+        ((JavascriptExecutor) driver).executeScript("mobile: dragGesture", ImmutableMap.of(
+                "elementId", ((RemoteWebElement) droppableElem).getId(),
+                "endX", 602,
+                "endY", 526
+        ));
+
+        String droppedText = driver.findElement(By.id("io.appium.android.apis:id/drag_result_text")).getText();
+        Assert.assertEquals(droppedText, "Dropped!");
+
+    }
+}
