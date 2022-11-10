46f15f36daf4f69cbe75062161467316ff331de1
ZeeshanTahir786 <hafizzeeshan138@gmail.com>
2022-11-10 16:31:52 +0500

    Added  Explicit wait

--- src/test/java/appium/eCommerceApp/TestCase1.java
+++ src/test/java/appium/eCommerceApp/TestCase1.java
@@ -3,8 +3,13 @@ package appium.eCommerceApp;
 import appium.BaseTest;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class TestCase1 extends BaseTestEcom {
     @Test
@@ -33,6 +38,15 @@ public class TestCase1 extends BaseTestEcom {
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
         Thread.sleep(3000);
 
+//      Explicit wait => Selenium support mvn repo required for using it.
+        WebDriverWait waitt = new WebDriverWait(driver, Duration.ofSeconds(5));
+        waitt.until(ExpectedConditions.attributeContains(
+                driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+
+        String lastPageProduct = driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText();
+        Assert.assertEquals(lastPageProduct, "PG 3");
+
+//        Code for handling toast
 //        String toastMessage =  driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
 //        Assert.assertEquals(toastMessage,"Please enter your name");
     }
