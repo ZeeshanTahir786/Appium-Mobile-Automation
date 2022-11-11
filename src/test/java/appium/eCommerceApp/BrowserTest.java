8e66621506862f88847423905262f49460338d0f
ZeeshanTahir786 <hafizzeeshan138@gmail.com>
2022-11-11 12:21:37 +0500

    Working on running browser test on mobile app

---
+++ src/test/java/appium/eCommerceApp/BrowserTest.java
@@ -0,0 +1,16 @@
+package appium.eCommerceApp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+public class BrowserTest extends BrowserBaseTestEcom {
+    @Test
+    public void browserTest() throws InterruptedException {
+        driver.get("https://www.google.com/");
+        System.out.println(driver.getTitle());
+        driver.findElement(By.name("q")).sendKeys("nisum.com");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
+    }
+}
