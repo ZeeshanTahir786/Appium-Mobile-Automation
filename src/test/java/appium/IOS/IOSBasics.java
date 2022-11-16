af669f494775b598bf9e5f5ed7f66bb8ce0ac1e9
ZeeshanTahir786 <hafizzeeshan138@gmail.com>
2022-11-16 18:11:14 +0500

    Play with  iosClassChain & IOSPredicateString

--- src/test/java/appium/IOS/IOSBasics.java
+++ src/test/java/appium/IOS/IOSBasics.java
@@ -5,9 +5,24 @@ import org.testng.annotations.Test;
 
 public class IOSBasics extends IOSBaseTest {
     @Test
-    public void basics(){
+    public void basics() {
 
         // Xpath, classname, IOS, iosClassCHain, IOSPredicateString, accessibility id, id
-        driver.findElement(AppiumBy.accessibilityId("Alert Views")).click();
+//      driver.findElement(AppiumBy.accessibilityId("Alert Views")).click();
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeStaticText[`label == 'Alert Views'`]")).click();
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeStaticText[`label == 'Text Entry'`]")).click();
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeOther")).sendKeys("Hafiz Zeeshan");
+        driver.findElement(AppiumBy.accessibilityId("OK")).click();
+
+//        driver.findElement(AppiumBy.iOSNsPredicateString("label == 'Confirm / Cancel'"));
+//        not for case senstive
+//        driver.findElement(AppiumBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' AND label == 'Confirm / Cancel' AND value BEGINSWITH 'Confirm'")).click();
+//        for case sensetive simply put [c] on BEGINSWITH
+        driver.findElement(AppiumBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' AND value BEGINSWITH[c] 'Confirm'")).click();
+//        driver.findElement(AppiumBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' AND value ENDSWITH[c] 'Confirm'")).click();
+
+        String txt = driver.findElement(AppiumBy.iOSNsPredicateString("label BEGINSWITH 'A message'")).getText();
+        System.out.println(txt);
+        driver.findElement(AppiumBy.iOSNsPredicateString("label == 'Confirm'")).click();
     }
 }
